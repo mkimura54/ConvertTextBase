d3d1b3e9dd64a65792b597935ebde1f7dd337766
Masashi Kimura <screw@mtg.biglobe.ne.jp>
2016-03-23 22:39:57 +0900

    Create New

---
+++ ConvertTextBase/ConvertText.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertTextBase
+{
+    public class ConvertText
+	{
+		private Encoding srcEncording;
+
+		private Encoding destEncording;
+
+		private StreamReader srcReader = null;
+
+		private StreamWriter destWriter = null;
+
+		public ConvertText()
+		{
+			srcEncording = Encoding.UTF8;
+			destEncording = Encoding.UTF8;
+		}
+
+		public bool Open(string srcFilePath, string destFilePath)
+		{
+			srcReader = new StreamReader(srcFilePath, srcEncording);
+			destWriter = new StreamWriter(destFilePath, false, destEncording);
+
+			return true;
+		}
+
+		public string ReadText()
+		{
+			return srcReader.ReadLine();
+		}
+
+		public bool WriteText(string text)
+		{
+			destWriter.WriteLine(text);
+			return true;
+		}
+		
+		public bool Close()
+		{
+			bool result = true;
+			try
+			{
+				if (srcReader != null) srcReader.Close();
+			}
+			catch
+			{
+				result = false;
+			}
+
+			try
+			{
+				if (destWriter != null) destWriter.Close();
+			}
+			catch
+			{
+				result = false;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConvertTextBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var convertText = new ConvertText();
+			if (convertText.Open("../../App.config", "sample.txt"))
+			{
+				while (true)
+				{
+					// input
+					var line = convertText.ReadText();
+					if (line == null) break;
+
+					// convert
+					var convertedText = line.Replace('<', '[').Replace('>', ']');
+
+					// output
+					convertText.WriteText(convertedText);
+				}
+				convertText.Close();
+			}
+		}
+	}
+}
