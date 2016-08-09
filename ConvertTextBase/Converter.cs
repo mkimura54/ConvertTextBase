8cb71354144bca4650213031285ef10bf828ec02
Masashi Kimura <screw@mtg.biglobe.ne.jp>
2016-08-09 23:02:41 +0900

    Add Text Converter Wrapper Class

---
+++ ConvertTextBase/Converter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConvertTextBase
+{
+	/// <summary>
+	/// Text Converter Wrapper
+	/// </summary>
+	public class Converter
+	{
+		/// <summary>
+		/// Text Converter
+		/// </summary>
+		private ConvertText convertText = new ConvertText();
+
+		/// <summary>
+		/// Write Text To Destination File Delegate
+		/// </summary>
+		/// <param name="readText">Readed Line Text</param>
+		/// <returns>Write Text Result</returns>
+		public delegate bool OutputText(string readText);
+
+		/// <summary>
+		/// Set Source File And Destination File Encodings
+		/// </summary>
+		/// <param name="encoding">Encoding</param>
+		public void SetEncoding(Encoding encoding)
+		{
+			convertText.SetEncoding(encoding);
+		}
+
+		/// <summary>
+		/// Fetch Line Text Event
+		/// </summary>
+		/// <param name="srcFilePath">Source File Path</param>
+		/// <param name="destFilePath">Destination File Path</param>
+		/// <param name="convertLogic">User's Convert Logic Delegate</param>
+		public void Fetch(string srcFilePath, string destFilePath,
+						  Action<string, OutputText> convertLogic)
+		{
+			if (convertText.Open(srcFilePath, destFilePath))
+			{
+				while (true)
+				{
+					// input
+					var line = convertText.ReadText();
+					if (line == null) break;
+
+					// convert and output
+					convertLogic(line, writeText);
+				}
+				convertText.Close();
+			}
+		}
+
+		/// <summary>
+		/// Write Line Text To Destination File
+		/// </summary>
+		/// <param name="text">Write Line Text</param>
+		/// <returns>Result</returns>
+		private bool writeText(string text)
+		{
+			return convertText.WriteText(text);
+		}
+	}
+}
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -11,6 +11,12 @@ namespace Sample
 	{
 		static void Main(string[] args)
 		{
+			sample1();
+			sample2();
+		}
+
+		private static void sample1()
+		{
 			var convertText = new ConvertText();
 			convertText.SetEncoding(Encoding.UTF8);
 			if (convertText.Open("../../App.config", "sample.txt"))
@@ -29,6 +35,22 @@ namespace Sample
 				}
 				convertText.Close();
 			}
+		}
+
+		private static void sample2()
+		{
+			Converter converter = new Converter();
+			converter.SetEncoding(Encoding.UTF8);
+			converter.Fetch("../../App.config", "sample.txt", sample2_fetch_event);
+		}
+
+		private static void sample2_fetch_event(string readLine, Converter.OutputText output)
+		{
+			// convert
+			var convertedText = readLine.Replace('<', '[').Replace('>', ']');
+
+			// output
+			var result = output(convertedText);
 		}
 	}
 }
